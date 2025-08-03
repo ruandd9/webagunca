d9750b7a8306761ea0b0ca5a288a6da9b8120f17
YanFellippe <yangomesbasilio@gmail.com>
2025-08-03 20:32:25 -0300

    feat: conexao entre usuarios e respetivos quadros + adicao de temas de auto contraste + aumento da fonte

--- backend/server.js
+++ backend/server.js
@@ -40,10 +40,4 @@ app.use('/api/card-labels', cardLabelRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-
-<<<<<<< HEAD
-}); 
-=======
-}); 
-
->>>>>>> 0295c0dead3f4faf083c4b795022ccde30e36096
+});
